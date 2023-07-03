160a6ebad5181cbfcff1ea055adc54dbc5834d53
Vitalii Vectortool <bychevoyvitaliy@gamil.com>
2023-07-03 12:52:35 +0300

    css 03.07.23 2

--- geometry.py
+++ geometry.py
@@ -37,7 +37,7 @@ class Perimeter():
         pass
 
     def rectangle(side_a: float, side_b: float) -> float:
-        return (side_a * 2) + (side_b * 2)
+        return round((side_a * 2) + (side_b * 2), 2)
 
     def rectangle_one_radius(side_a: float, side_b: float, radius: float) -> float:
         return round((radius * 2 * 3.1415) + ((side_a - (2 * radius)) * 2) + ((side_b - (2 * radius)) * 2), 1)
