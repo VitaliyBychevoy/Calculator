d1adf80ba0a88f35461c529632d24758456639f7
Vitalii Vectortool <bychevoyvitaliy@gamil.com>
2023-07-17 16:43:50 +0300

    1

--- test_geometry.py
+++ test_geometry.py
@@ -7,24 +7,32 @@ class RoundTest(unittest.TestCase):
     def setUp(self) -> None:
         self.r = Round()
 
-    def test_round_positive(self):
+    def test_round_the_smallest_value(self):
+        self.assertEqual(self.r.perimeter_round(0.01), 0.03)
+
+    def test_round_positive_float(self):
+        self.assertEqual(self.r.perimeter_round(30.0), 94.25)
+
+    def test_round_positive_int(self):
         self.assertEqual(self.r.perimeter_round(30), 94.25)
     
-    def test_round_positive_tipe_float(self):
-        self.assertEqual(type(self.r.perimeter_round(30)),float)
+    def test_round_notEqual(self):
+        self.assertNotEqual(self.r.perimeter_round(20.8), 3)
 
-    def test_round_negative_type_str(self):
-        self.assertNotEqual(type(self.r.perimeter_round(30)), str)
+    def test_negative_none_value(self):
+        self.assertRaises(TypeError, self.r.perimeter_round, None)
 
-    def test_round_negative_type_bool(self):
-        self.assertNotEqual(type(self.r.perimeter_round(30)), bool)
+    def test_negative_value_negative_number(self):
+        self.assertRaises(ValueError, self.r.perimeter_round, -3)
+
+    def test_negative_value(self):
+        self.assertRaises(TypeError, self.r.perimeter_round, [2])
 
     def test_type_input(self):
-        self.assertRaises(ValueError, self.r.perimeter_round, "f")
+        self.assertRaises(TypeError, self.r.perimeter_round, "f")
 
     def tearDown(self) -> None:
         del(self.r)
-
 
 
 if __name__ == "__main__":
