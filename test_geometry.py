9c172319a52c22f7fafcd488cbb7405f2a263dee
Vitalii Vectortool <bychevoyvitaliy@gamil.com>
2023-07-18 17:04:17 +0300

    unittest Incomplete_circle

--- test_geometry.py
+++ test_geometry.py
@@ -7,6 +7,7 @@ class RoundTest(unittest.TestCase):
     def setUp(self) -> None:
         self.r = Round()
 
+    #test primeter
     def test_round_the_smallest_value(self):
         self.assertEqual(self.r.perimeter_round(0.01), 0.03)
 
@@ -31,9 +32,39 @@ class RoundTest(unittest.TestCase):
     def test_type_input(self):
         self.assertRaises(TypeError, self.r.perimeter_round, "f")
 
+    def test_round_miss_argumetn(self):
+        self.assertRaises(TypeError, self.r.perimeter_round)
+
+    #test shape group 
+    def test_group_shape(self):
+        self.assertEqual(self.r.get_class_shape(), "Стандартна форма")
+
     def tearDown(self) -> None:
         del(self.r)
 
+
+class Incomplete_circle_test(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.inc_cir = Incomplete_circle()
+
+    def test_incomplete_circle_positive(self):
+        self.assertEqual(self.inc_cir.perim_in_circle(60, 45), 177.62)
+
+    def test_incomplete_circle_negative_miss_argument(self):
+        self.assertRaises(TypeError, self.inc_cir.perim_in_circle, 3)
+
+    def test_incomplete_circle_negative_miss_argument_height(self):
+        self.assertRaises(TypeError, self.inc_cir.perim_in_circle, diameter=34)
+
+    def test_incomplete_circle_negative_miss_argument_diameter(self):
+        self.assertRaises(TypeError, self.inc_cir.perim_in_circle, height=5)
+
+    def test_incomplete_circle_negative_miss_arguments(self):
+        self.assertRaises(TypeError, self.inc_cir.perim_in_circle)
+
+    def tearDown(self) -> None:
+        del(self.inc_cir)
 
 if __name__ == "__main__":
     unittest.main()
